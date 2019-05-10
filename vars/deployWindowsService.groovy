f5995242d0e0c2594a90b2350817cc393720a767
alberto bobice <abobice@sorint.it>
2019-05-10 15:22:29 +0200

    add calling powershell script to generate xml file necessary to create windows service

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -8,9 +8,10 @@ def call(Map params){
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: GlobalVars.urlRepositoryWindowScript]]])
     fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${params.installationDir}")])
     dir("${params.installationDir}"){
+        powershell label: '', script: "./deploy_service.ps1 -serviceName ${params.serviceName} -jarName ${params.jarName}"
         //uninstall the service if it is already present
-        bat ".\\${params.serviceName}.exe uninstall"
-        bat ".\\${params.serviceName}.exe install"
+        bat ".\\${params.serviceName} uninstall"
+        bat ".\\${params.serviceName} install"
     }
 }
 
