a7fd84951936ea425817c06c200054a4d7252f46
alberto bobice <abobice@sorint.it>
2019-05-10 10:48:26 +0200

    add variable containing url git repository with scripts containing windows services

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -2,16 +2,16 @@
 
 import com.carel.GlobalVars
 
-def call(String serviceName, String jarName, String installationDir) {
+//def call(String serviceName, String jarName, String installationDir) {
+def call(Map params){
+    echo "Installing ${params.jarName}.jar under  ${installationDir} as windows service named ${params.serviceName}"
     echo "Download script to install jar as windows service from ${GlobalVars.urlRepositoryWindowScript}"
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: GlobalVars.urlRepositoryWindowScript]]])
-    fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${installationDir}")])
-    echo "Install ${jarName} as windows service"
+    fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${params.installationDir}")])
     dir("${installationDir}"){
-        powershell label: '', script: "./deploy_service.ps1 -serviceName ${serviceName} -jarName ${jarName}"
         //uninstall the service if it is already present
-        bat ".\\${serviceName} uninstall"
-        bat ".\\${serviceName} install"
+        bat ".\\${params.serviceName} uninstall"
+        bat ".\\${params.serviceName} install"
     }
 }
 
