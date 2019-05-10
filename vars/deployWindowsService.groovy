c10c1cf036fb488ab94267068bbf208b3a23a648
alberto bobice <abobice@sorint.it>
2019-05-10 09:16:10 +0200

    first test pipeline library

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -5,7 +5,7 @@ def call(String serviceName, String jarName, String installationDir) {
     fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${installationDir}")])
     dir("${installationDir}"){
         powershell label: '', script: "./deploy_service.ps1 -serviceName ${serviceName} -jarName ${jarName}"
-        //unistall the service if it is already present
+        //uninstall the service if it is already present
         bat ".\\${serviceName} uninstall"
         bat ".\\${serviceName} install"
     }
