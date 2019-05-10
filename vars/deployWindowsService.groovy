0abf07c7586fc1714918caaaa9310c055db39a27
alberto bobice <abobice@sorint.it>
2019-05-10 09:49:41 +0200

    add windows script downloaded from git

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -1,8 +1,13 @@
 #!/usr/bin/env groovy
 
+
+
+
 def call(String serviceName, String jarName, String installationDir) {
-    echo 'Install as windows service'
+
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/diospada/scriptToDeployJarAsWindowsService.git']]])
     fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${installationDir}")])
+    echo 'Install as windows service'
     dir("${installationDir}"){
         powershell label: '', script: "./deploy_service.ps1 -serviceName ${serviceName} -jarName ${jarName}"
         //uninstall the service if it is already present
