3554235f3c2dce188fed006b7109c94ab33ae9b4
Alberto Bobice <alberto.bobice@gmail.com>
2019-05-20 14:20:32 +0200

    Update deployWindowsService.groovy

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -10,8 +10,8 @@ def call(Map params){
     dir("${params.installationDir}"){
         powershell label: '', script: "./deploy_service.ps1 -serviceName ${params.serviceName} -jarName ${params.jarName}"
         //uninstall the service if it is already present
-        bat ".\\${params.serviceName} uninstall"
-        bat ".\\${params.serviceName} install"
+        //bat ".\\${params.serviceName} uninstall"
+        //bat ".\\${params.serviceName} install"
     }
 }
 
