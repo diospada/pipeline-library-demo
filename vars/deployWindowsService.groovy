4a45b133c73891a4c41f48d75b45a624a13edf82
alberto bobice <abobice@sorint.it>
2019-06-06 17:22:31 +0200

    clean up

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -9,9 +9,6 @@ def call(Map params){
     fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${params.installationDir}")])
     dir("${params.installationDir}"){
         powershell label: '', script: "./deploy_service.ps1 -serviceName ${params.serviceName} -jarName ${params.jarName}"
-        //uninstall the service if it is already present
-        //bat ".\\${params.serviceName} uninstall"
-        //bat ".\\${params.serviceName} install"
     }
 }
 
