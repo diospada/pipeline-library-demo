ee91e764f086bf9aa588ff45f05c9d8fd6cf0611
alberto bobice <abobice@sorint.it>
2019-06-06 17:18:03 +0200

    test

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -4,7 +4,7 @@ import com.carel.GlobalVars
 
 def call(Map params){
     echo "Test"
-    String installationDirectory =   ${params.installationDir} + File.separator + ${params.serviceName}
+    String installationDirectory =   "${params.installationDir}" + File.separator + "${params.serviceName}"
     echo "Installing ${params.jarName}.jar under ${installationDirectory} as windows service named ${params.serviceName}"
     echo "Download script to install jar as windows service from ${GlobalVars.urlRepositoryWindowScript}"
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: GlobalVars.urlRepositoryWindowScript]]])
