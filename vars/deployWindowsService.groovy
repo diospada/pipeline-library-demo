8e3e5170a4c27e22d3cfc7b780dd9f7479d3dafa
alberto bobice <abobice@sorint.it>
2019-06-06 16:58:16 +0200

    change installationDirectoryMicroservice

--- vars/deployWindowsService.groovy
+++ vars/deployWindowsService.groovy
@@ -3,15 +3,13 @@
 import com.carel.GlobalVars
 
 def call(Map params){
-    echo "Installing ${params.jarName}.jar under  ${params.installationDir} as windows service named ${params.serviceName}"
+    String installationDirectory =   ${params.installationDir} + File.separator + ${params.serviceName}
+    echo "Installing ${params.jarName}.jar under ${installationDirectory} as windows service named ${params.serviceName}"
     echo "Download script to install jar as windows service from ${GlobalVars.urlRepositoryWindowScript}"
     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: GlobalVars.urlRepositoryWindowScript]]])
-    fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${params.installationDir}")])
-    dir("${params.installationDir}"){
+    fileOperations([fileCopyOperation(excludes: '', flattenFiles: true, includes: "windows/*.*", targetLocation: "${installationDirectory}")])
+    dir("${installationDirectory}"){
         powershell label: '', script: "./deploy_service.ps1 -serviceName ${params.serviceName} -jarName ${params.jarName}"
-        //uninstall the service if it is already present
-        //bat ".\\${params.serviceName} uninstall"
-        //bat ".\\${params.serviceName} install"
     }
 }
 
