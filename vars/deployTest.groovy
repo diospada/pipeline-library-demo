9e5f632b990c1e0342bccfefe66e76b12b501886
alberto bobice <abobice@sorint.it>
2019-06-06 12:58:16 +0200

    test parameter existence

--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -4,7 +4,18 @@ import com.carel.GlobalVars
 
  def call(Map params){
 
-     echo "Installing ${params.jarName}.jar under  ${params.installationDir} as linux service named ${params.serviceName}"
+     Boolean missinParameter = false
+     for (String param in ["installationDir", "serviceName", "jarName"]) {
+         if (params.containsKey(param)) {
+             echo "Missing parameter ${param}"
+             missinParameter = true
+         }
+     }
+     if(missinParameter){
+         System.exit(0)
+     }else {
+         echo "Installing ${params.jarName}.jar under  ${params.installationDir} as linux service named ${params.serviceName}"
+     }
 
  }
 
